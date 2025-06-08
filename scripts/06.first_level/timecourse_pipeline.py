131b23d84481ff0811a6bc940246c4acdb347794
Melissa Thye <melissathye@gmail.com>
2025-06-08 13:27:10 +0100

    add group ROI opt to timecourse pipeline

--- scripts/06.first_level/timecourse_pipeline.py
+++ scripts/06.first_level/timecourse_pipeline.py
@@ -171,6 +171,13 @@ def create_timecourse_workflow(sharedDir, projDir, derivDir, workDir, outDir, su
                     roi_masks.append(roi_file)
                     print('Using {} fROI file from {}'.format(roi_name, roi_file))
             
+            # if group ROI was specified
+            elif 'group' in m:
+                roi_name = m.split('group-')[1]
+                roi_file = glob.glob(op.join(projDir, 'files', 'ROIs' , '{}*.nii.gz'.format(roi_name)))[0]
+                roi_masks.append(roi_file)
+                print('Using {} group defined file from {}'.format(roi_name, roi_file))  
+            
             # if a freesurfer ROI was specified
             elif 'FS' in m:
                 roi_name = m.split('FS-')[1]

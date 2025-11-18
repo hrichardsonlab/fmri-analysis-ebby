b05a31a089e06379d686ab4bdafc6d23389c423c
Melissa Thye <melissathye@gmail.com>
2025-11-18 10:32:33 +0000

    update fmriprep script to require config file

--- scripts/04.fmriprep/run_fmriprep.sh
+++ scripts/04.fmriprep/run_fmriprep.sh
@@ -13,53 +13,85 @@ Usage() {
 	echo
 	echo
 	echo "Usage:"
-	echo "./run_fmriprep.sh <list of subjects>"
+	echo "./run_fmriprep.sh <config file> <list of subjects>"
 	echo
 	echo "Example:"
-	echo "./run_fmriprep.sh KMVPA_subjs.txt"
+	echo "./run_fmriprep.sh config-pixar_mind-body.tsv open-pixar_subjs.txt"
 	echo
-	echo "KMVPA_subjs.txt is a file containing the participants to run fMRIPrep on:"
-	echo "001"
-	echo "002"
+	echo "open-pixar_subjs.txt is a file containing the participants to run fMRIPrep on:"
+	echo "sub-pixar001"
+	echo "sub-pixar002"
 	echo "..."
 	echo
 	echo
 	echo "This script must be run within the /RichardsonLab/ directory on the server due to space requirements."
 	echo "The script will terminiate if run outside of the /RichardsonLab/ directory."
 	echo
-	echo "Script created by Manuel Blesa & Melissa Thye"
+	echo "Script created by Melissa Thye"
 	echo
 	exit
 }
-[ "$1" = "" ] && Usage
+[ "$1" = "" ] | [ "$2" = "" ] && Usage
 
 # if the script is run outside of the RichardsonLab directory (e.g., in home directory where space is limited), terminate the script and show usage documentation
 if [[ ! "$PWD" =~ "/RichardsonLab/" ]]; 
 then Usage
 fi
 
+if [ ! ${1##*.} == "tsv" ]
+then
+	echo
+	echo "The configuration file was not found."
+	echo "The script must be submitted with (1) a configuration file name and (2) a subject-run list as in the example below."
+	echo
+	echo "./run_fmriprep.sh config-pixar_mind-body.tsv open-pixar_subjs.txt"
+	echo	
+	# end script and show full usage documentation	
+	Usage
+fi
+
+if [ ! ${2##*.} == "txt" ]
+then
+	echo
+	echo "The list of participants was not found."
+	echo "The script must be submitted with (1) a configuration file name and (2) a subject-run list as in the example below."
+	echo
+	echo "./run_fmriprep.sh config-pixar_mind-body.tsv open-pixar_subjs.txt"
+	echo	
+	# end script and show full usage documentation	
+	Usage
+fi
+
 # define directories
 projDir=`cat ../../PATHS.txt`
 singularityDir="${projDir}/singularity_images"
 
-# define subjects from text document
-subjs=$(cat $1 | awk '{print $1}') 
-
-# extract study name from list of subjects filename
-study=` basename $1 | cut -d '_' -f 1 `
+# define config file and subjects from files passed in script call
+config=${projDir}/$1
+subjs=$(cat $2 | awk '{print $1}')
 
 # define data directories depending on study information
-bidsDir="/RichardsonLab/preprocessedData/${study}"
-derivDir="${bidsDir}/derivatives"
+sharedDir=$(awk -F'\t' '$1=="sharedDir"{print $2}' "$config")
+bidsDir=$(awk -F'\t' '$1=="bidsDir"{print $2}' "$config")
+derivDir=$(awk -F'\t' '$1=="derivDir"{print $2}' "$config")
+
+# extract preprocessing relevant values from config file
+multiecho=$(awk -F'\t' '$1=="multiecho"{print $2}' "$config")
+
+# strip extra formatting if present
+sharedDir="${sharedDir%$'\r'}"
+bidsDir="${bidsDir%$'\r'}"
+derivDir="${derivDir%$'\r'}"
+multiecho="${multiecho%$'\r'}"
+
+# export freesurfer license file location
+export license=${sharedDir}/tools/license.txt
 
 # create derivatives directory if it doesn't exist
 if [ ! -d ${derivDir} ]
 then 
 	mkdir -p ${derivDir}
 fi
-
-# export freesurfer license file location
-export license=/RichardsonLab/processing/tools/license.txt
 
 # change the location of the singularity cache ($HOME/.singularity/cache by default, but limited space in this directory)
 export SINGULARITY_TMPDIR=${singularityDir}
@@ -77,39 +109,69 @@ echo "${subjs}"
 # iterate for all subjects in the text file
 while read p
 do
-	NAME=` basename ${p} | awk -F- '{print $NF}' `	# subj number from folder name
+	sub=` basename ${p} `
 
 	echo
-	echo "Running fMRIprep for sub-${NAME}"
+	echo "Running fMRIprep for ${sub}"
 	echo
 	
 	# make output subject derivatives directory
-	mkdir -p ${derivDir}/sub-${NAME}
-
-	# run singularity
-	singularity run -B /RichardsonLab:/RichardsonLab,${singularityDir}:/opt/templateflow \
-	${singularityDir}/fmriprep-24.0.0.simg  							\
-	${bidsDir} ${derivDir}												\
-	participant															\
-	--participant-label ${NAME}											\
-	--skip_bids_validation												\
-	--nthreads 8														\
-	--omp-nthreads 4													\
-	--ignore slicetiming												\
-	--fd-spike-threshold 1												\
-	--dvars-spike-threshold 1.5											\
-	--output-space MNI152NLin2009cAsym:res-2 T1w						\
-	--return-all-components												\
-	--derivatives ${derivDir}											\
-	--stop-on-first-crash												\
-	-w ${singularityDir}												\
-	--fs-license-file ${license}  > ${derivDir}/sub-${NAME}/log_fmriprep_sub-${NAME}.txt
-
+	mkdir -p ${derivDir}/${sub}
+	
+	# run fmriprep depending on whether data are multi-echo
+	if [[ "${multiecho}" == "yes" ]]
+	then
+		echo "Data were aquired with a multi-echo sequence. Running multi-echo fMRIPrep command..."
+		
+		# run singularity
+		singularity run -B /RichardsonLab:/RichardsonLab,${singularityDir}:/opt/templateflow	\
+		${singularityDir}/fmriprep-24.0.0.simg													\
+		${bidsDir} ${derivDir}																	\
+		participant																				\
+		--participant-label ${sub}																\
+		--skip_bids_validation																	\
+		--nthreads 8																			\
+		--omp-nthreads 4																		\
+		--ignore slicetiming																	\
+		--fd-spike-threshold 1																	\
+		--dvars-spike-threshold 1.5																\
+		--me-t2s-fit-method curvefit															\
+		--me-output-echos																		\
+		--output-space MNI152NLin2009cAsym:res-2 T1w											\
+		--return-all-components																	\
+		--derivatives ${derivDir}																\
+		--stop-on-first-crash																	\
+		-w ${singularityDir}																	\
+		--fs-license-file ${license}  > ${derivDir}/${sub}/log_fmriprep_${sub}.txt
+	# if data were not acquired with multiecho sequence
+	else
+		echo "Data were not acquired with a multi-echo sequence. Running default fMRIPrep command..."
+		
+		# run singularity
+		singularity run -B /RichardsonLab:/RichardsonLab,${singularityDir}:/opt/templateflow	\
+		${singularityDir}/fmriprep-24.0.0.simg													\
+		${bidsDir} ${derivDir}																	\
+		participant																				\
+		--participant-label ${sub}																\
+		--skip_bids_validation																	\
+		--nthreads 8																			\
+		--omp-nthreads 4																		\
+		--ignore slicetiming																	\
+		--fd-spike-threshold 1																	\
+		--dvars-spike-threshold 1.5																\
+		--output-space MNI152NLin2009cAsym:res-2 T1w											\
+		--return-all-components																	\
+		--derivatives ${derivDir}																\
+		--stop-on-first-crash																	\
+		-w ${singularityDir}																	\
+		--fs-license-file ${license}  > ${derivDir}/${sub}/log_fmriprep_${sub}.txt
+	fi
+	
 	# move subject report and freesurfer output files to appropriate directories
 	mv ${derivDir}/*dseg.tsv ${derivDir}/sourcedata/freesurfer
-	#mv ${derivDir}/sub-${NAME}.html ${derivDir}/sub-${NAME}
+	#mv ${derivDir}/${sub}.html ${derivDir}/${sub}
 	
 	# give other users permissions to created files
-	#chmod -R a+wrx ${derivDir}/sub-${NAME}
+	#chmod -R a+wrx ${derivDir}/${sub}
 
-done <$1
+done <$2
